1d0eb988bb87e5880b7b5c2d9ca9d0bdb54c4dd9
Chijioke Okafo <cokafo@innocean.ca>
2018-02-21 00:08:30 -0500

    Starting to add tileflip to home page, and adding to other pages

--- js/main.js
+++ js/main.js
@@ -4,14 +4,25 @@ $(document).ready(function(){
       $(".nav-container").toggleClass("nav-open"); 
   });
 
-  $("ul.nav-container").find("a").click(function(e) {
-      e.preventDefault();
-      var section = $(this).attr("href");
-      $("html, body").animate({
-          scrollTop: $(section).offset().top
+  // $("ul.nav-container").find("a").click(function(e) {
+  //     e.preventDefault();
+  //     var section = $(this).attr("href");
+  //     $("html, body").animate({
+  //         scrollTop: $(section).offset().top
+  //     });
+
+  //     $(".nav-container").toggleClass("nav-open");
+  // });
+
+
+  $('.tile-flip').each(function(){
+    var flipContainer = $(this);
+    $('.flip-container', flipContainer).each(function(){
+      $(this).click(function(){
+        var wasOpen = $('.flipper', this).hasClass('flip');
+        $('.flipper', flipContainer).removeClass('flip');
+        if(!wasOpen) $('.flipper', this).addClass('flip');
       });
-
-      $(".nav-container").toggleClass("nav-open");
+    });
   });
-
 });
